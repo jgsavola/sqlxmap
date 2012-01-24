e71d07b5b6336f13327cf851fedcbfd39b457f21
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-01-24 08:55:09 +0200

    Lisää JUnit-testit Settings-luokalle
    Nyt testataan vain sitä, että tietokantayhteyden tiedot todella
    tallennetaan käyttäjän kotihakemistoon.

---
+++ test/sqlxmap/SettingsTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sqlxmap;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jonne
+ */
+public class SettingsTest {
+    String tempDir;
+    
+    public SettingsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+        tempDir = System.getProperty("java.io.tmpdir");
+    }
+    
+    @After
+    public void tearDown() {
+        new File(tempDir, ".sqlxmap/databases.properties").delete();
+        new File(tempDir, ".sqlxmap").delete();
+    }
+
+
+    @Test
+    public void testSettingsAreStored() {
+        Settings settings;
+
+        DatabaseInfo dbInfo = new DatabaseInfo("testhost", 9999, "testdb", "testuser", "testpass");
+        storeDatabaseSettings(dbInfo);
+        
+        try {
+            settings = new Settings(tempDir);
+        } catch (Exception ex) {
+            Logger.getLogger(SettingsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Loading of Settings failed");
+            return;
+        }
+
+        DatabaseInfo dbInfo2 = settings.getDbInfo().get(0);
+
+        assertTrue("Database settings are persistent.",
+                dbInfo.toString().equals(dbInfo2.toString()));
+    }
+    
+    private void storeDatabaseSettings(DatabaseInfo dbInfo) {
+        Settings settings;
+        try {
+            settings = new Settings(tempDir);
+        } catch (Exception ex) {
+            Logger.getLogger(SettingsTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Loading of Settings failed");
+            return;
+        }
+
+        ArrayList<DatabaseInfo> databases = new ArrayList<DatabaseInfo>();
+        databases.add(dbInfo);
+        settings.setDbInfo(databases);
+    }
+          
+}
