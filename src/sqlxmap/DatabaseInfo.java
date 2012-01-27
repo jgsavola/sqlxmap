19b768a1ecea3e2f4d43cc835cbd9d00b454d406
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-01-27 10:46:34 +0200

    Lisää javadoc-kommentit DatabaseInfo-luokkaan
    Kommentit suomeksi. Pitäisi varmaankin kääntää loppukin ohjelmasta
    suomeksi ja jatkaa sillä.

--- src/sqlxmap/DatabaseInfo.java
+++ src/sqlxmap/DatabaseInfo.java
@@ -6,15 +6,42 @@ package sqlxmap;
 
 /**
  *
+ * Tietokantayhteyden parametrit.
+ * 
  * @author jonne
+ * 
  */
 class DatabaseInfo {
+    /**
+     * Tietokantaklusterin isäntäkoneen nimi tai IP-osoite (oletus localhost).
+     */
     private String dbHost;
+    
+    /**
+     * Tietokantaklusterin portti (oletus 5432).
+     */
     private int dbPort;
+    
+    /**
+     * Tietokannan nimi (oletus käyttäjän login-tunnus).
+     */
     private String dbName;
+    
+    /**
+     * Tietokantaklusterin käyttäjätunnus (oletus käyttäjän login-tunnus).
+     */
     private String dbUser;
+    
+    /**
+     * Tietokantaklusterin salasana (oletus "").
+     */
     private String dbPassword;
 
+    /**
+     * Luokan konstruktori.
+     * 
+     * Oletusarvot: dbHost=localhost, dbPort=5432, dbName=user.name, dbUser=user.name, dbPassword=""
+     */
     public DatabaseInfo() {
         dbHost = "localhost";
         dbPort = 5432;
@@ -23,6 +50,15 @@ class DatabaseInfo {
         dbPassword = "";
     }
 
+    /**
+     * Luokan konstruktori kaikilla parametreilla.
+     * 
+     * @param dbHost
+     * @param dbPort
+     * @param dbName
+     * @param dbUser
+     * @param dbPassword 
+     */
     public DatabaseInfo(String dbHost, int dbPort, String dbName, String dbUser, String dbPassword) {
         this.dbHost = dbHost;
         this.dbPort = dbPort;
@@ -31,47 +67,102 @@ class DatabaseInfo {
         this.dbPassword = dbPassword;
     }
 
+    /**
+     * Antaa tietokantaklusterin isäntäkoneen.
+     * 
+     * @return Tietokantaklusterin isäntäkone.
+     */
+    
     public String getDbHost() {
         return dbHost;
     }
 
+    /**
+     * Muodostaa helposti luettavan teksti-muotoisen esityksen parametreista.
+     * 
+     * @return Tekstimuotoinen esitys.
+     */
     @Override
     public String toString() {
         return "DatabaseInfo{" + "dbHost=" + dbHost + ", dbPort=" + dbPort + ", dbName=" + dbName + ", dbUser=" + dbUser + ", dbPassword=" + dbPassword + '}';
     }
 
+    /**
+     * Asettaa tietokantaklusterin isäntäkoneen.
+     * 
+     * @param dbHost Tietokantaklusterin isäntäkone.
+     */
     public void setDbHost(String dbHost) {
         this.dbHost = dbHost;
     }
 
+    /**
+     * Antaa tietokannan nimen.
+     * 
+     * @return Tietokannan nimi.
+     */
     public String getDbName() {
         return dbName;
     }
 
+    /**
+     * Asettaa tietokannan nimen.
+     * 
+     * @param dbName Tietokannan nimi.
+     */        
     public void setDbName(String dbName) {
         this.dbName = dbName;
     }
 
+    /**
+     * Antaa tietokantaklusterin salasanan.
+     * 
+     * @return Tietokantaklusterin salasana.
+     */
     public String getDbPassword() {
         return dbPassword;
     }
 
+    /**
+     * Asettaa tietokantaklusterin salasanan.
+     * 
+     * @param dbName Tietokantalkusterin salasana.
+     */        
     public void setDbPassword(String dbPassword) {
         this.dbPassword = dbPassword;
     }
 
+    /** Antaa tietokantaklusterin portin.
+     * 
+     * @return Tietokantaklusterin portti.
+     */
     public int getDbPort() {
         return dbPort;
     }
 
+    /**
+     * Asettaa tietokantaklusterin portin.
+     * 
+     * @param dbPort Tietokantaklusterin portti.
+     */
     public void setDbPort(int dbPort) {
         this.dbPort = dbPort;
     }
 
+    /**
+     * Antaa tietokantaklusterin käyttäjätunnuksen.
+     * 
+     * @return Tietokantaklusterin käyttäjätunnus.
+     */
     public String getDbUser() {
         return dbUser;
     }
 
+    /**
+     * Asettaa tietokantaklusterin käyttäjätunnuksen.
+     * 
+     * @param dbUser Tietokantaklusterin käyttäjätunnus.
+     */
     public void setDbUser(String dbUser) {
         this.dbUser = dbUser;
     }
