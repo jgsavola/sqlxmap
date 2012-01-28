ce8feeb229b19ad1c62bb06bdfbf384ab097d364
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-01-28 12:32:44 +0200

    Päivitä LayerData:n testit
    Testaa addWKBGeometry-metodia.

--- test/sqlxmap/LayerDataTest.java
+++ test/sqlxmap/LayerDataTest.java
@@ -8,6 +8,8 @@ import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKBReader;
+import com.vividsolutions.jts.io.WKBWriter;
 import com.vividsolutions.jts.io.WKTReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,21 +23,28 @@ import static org.junit.Assert.*;
 public class LayerDataTest {
     private GeometryFactory gf = new GeometryFactory();
     private WKTReader wktReader = new WKTReader(gf);
+    private WKBReader wkbReader = new WKBReader(gf);
+    private WKBWriter wkbWriter = new WKBWriter();
 
     private String testWKT1;
     private String testWKT2;
+    private String testHexWKB1;
+    private byte[] testWKB1;
     private Geometry testGeometry1;
     private Geometry testGeometry2;
     
     public LayerDataTest() {
         testWKT1 = "POINT(100 100)";
         testWKT2 = "LINESTRING(0 0, 200 0, 200 200, 0 200, 0 0)";
+        
         try {
             testGeometry1 = wktReader.read(testWKT1);
             testGeometry2 = wktReader.read(testWKT2);
         } catch (ParseException ex) {
             Logger.getLogger(LayerDataTest.class.getName()).log(Level.SEVERE, null, ex);
         }
+        testWKB1    = wkbWriter.write(testGeometry1);
+        testHexWKB1 = WKBWriter.toHex(testWKB1);
     }
 
     @BeforeClass
@@ -119,5 +128,37 @@ public class LayerDataTest {
         // instance.extendEnvelope(geom);
         // // TODO review the generated test code and remove the default call to fail.
         // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addWKBGeometry method, of class LayerData.
+     */
+    @Test
+    public void testAddWKBGeometry_String() throws Exception {
+        System.out.println("addWKBGeometry");
+        LayerData instance = new LayerData();
+        instance.addWKBGeometry(testHexWKB1);
+        Envelope testEnvelope = new Envelope(100, 100, 100, 100);
+        
+        System.out.println("envelope: " + instance.getEnvelope());
+        
+        assertTrue("addWKBGeometry(String) extends layer's envelope.", 
+                instance.getEnvelope().toString().equals( testEnvelope.toString() ));
+    }
+
+    /**
+     * Test of addWKBGeometry method, of class LayerData.
+     */
+    @Test
+    public void testAddWKBGeometry_byteArr() throws Exception {
+        System.out.println("addWKBGeometry");
+        LayerData instance = new LayerData();
+        instance.addWKBGeometry(testWKB1);
+        Envelope testEnvelope = new Envelope(100, 100, 100, 100);
+        
+        System.out.println("envelope: " + instance.getEnvelope());
+        
+        assertTrue("addWKBGeometry(byte[]) extends layer's envelope.", 
+                instance.getEnvelope().toString().equals( testEnvelope.toString() ));
     }
 }
