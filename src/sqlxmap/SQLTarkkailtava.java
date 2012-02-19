a6ae181429118a6cdaca89d3dfc8f642cd287893
Jonne Savolainen <jonne.savolainen@helsinki.fi>
2012-02-19 13:25:37 +0200

    Lisää Javadoc SQLTarkkailtavaan

--- src/sqlxmap/SQLTarkkailtava.java
+++ src/sqlxmap/SQLTarkkailtava.java
@@ -7,19 +7,39 @@ package sqlxmap;
 import java.util.Observable;
 
 /**
+ * Tämän luokan avulla Observer-mallia käyttävät tarkkailijat saavat tiedon
+ * SQL-merkkijonon päivittymisestä.
+ *
+ * Luokkaa käytetään mm. SQL-kyselyikkunan ja karttanäytön tiedonsiirtoon.
  *
  * @author jonne
  */
 public class SQLTarkkailtava extends Observable {
     private String SQL;
 
+    /**
+     * Luo uusi <code>SQLTarkkailtava</code.
+     */
     public SQLTarkkailtava() {
     }
 
+    /**
+     * Anna SQL-merkkijono.
+     *
+     * @return SQL-merkkijono.
+     */
     public String getSQL() {
         return SQL;
     }
 
+    /**
+     * Aseta SQL-merkkijono.
+     *
+     * Tämä metodi kutsuu mahdollisia tarkkailijoita SQL-merkkijonon
+     * asettamisen jälkeen.
+     *
+     * @param SQL SQL-merkkijono.
+     */
     public void setSQL(String SQL) {
         this.SQL = SQL;
         setChanged();
